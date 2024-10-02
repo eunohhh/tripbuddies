b017fe4d672a394723e138af9a56df89ebae89c8
Eun Oh <bdohhhhh@gmail.com>
2024-10-02 21:33:57 +0900

    [update]: 기본 설정 업데이트 및 pnpm 으로 변경

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,28 @@
 module.exports = {
-    extends: ['next/core-web-vitals', 'plugin:prettier/recommended'],
-    // plugins: ['prettier', 'unused-imports'],
-    plugins: ['prettier'],
-    rules: {
-        // 선언되지 않은 변수 또는 임포트 구문 정리 규칙
-        'no-undef': 'off',
-        // 'unused-imports/no-unused-imports': 'error',
+  extends: ['next/core-web-vitals', 'plugin:prettier/recommended'],
+  // plugins: ['prettier', 'unused-imports'],
+  plugins: ['prettier'],
+  rules: {
+    // 선언되지 않은 변수 또는 임포트 구문 정리 규칙
+    'no-undef': 'off',
+    // 'unused-imports/no-unused-imports': 'error',
 
-        // 프리티어 설정
-        'prettier/prettier': [
-            'error',
-            {
-                singleQuote: true,
-                semi: true,
-                tabWidth: 4,
-                trailingComma: 'all',
-                printWidth: 80,
-                bracketSpacing: true,
-                arrowParens: 'avoid',
-                endOfLine: 'auto',
-            },
-        ],
-    },
-    globals: {
-        RequestInit: 'readonly',
-    },
+    // 프리티어 설정
+    // 'prettier/prettier': [
+    //     'error',
+    //     {
+    //         singleQuote: true,
+    //         semi: true,
+    //         tabWidth: 4,
+    //         trailingComma: 'all',
+    //         printWidth: 80,
+    //         bracketSpacing: true,
+    //         arrowParens: 'avoid',
+    //         endOfLine: 'auto',
+    //     },
+    // ],
+  },
+  globals: {
+    RequestInit: 'readonly',
+  },
 };
--- next.config.mjs
+++ next.config.mjs
@@ -3,72 +3,71 @@ const PHASE_PRODUCTION_BUILD = process.env.NODE_ENV === 'production';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: false,
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'pedixhwyfardtsanotrp.supabase.co',
-            },
-            {
-                protocol: 'http',
-                hostname: '**.kakaocdn.net',
-            },
-            {
-                protocol: 'https',
-                hostname: '**.googleusercontent.com',
-            },
-            {
-                protocol: 'https',
-                hostname: '**.pstatic.net',
-            },
-            { protocol: 'https', hostname: 'encrypted-tbn3.gstatic.com' },
-            { protocol: 'https', hostname: 'example.com' },
-        ],
-    },
-    webpack(config) {
-        // Grab the existing rule that handles SVG imports
-        const fileLoaderRule = config.module.rules.find(rule =>
-            rule.test?.test?.('.svg'),
-        );
-        config.module.rules.push(
-            {
-                ...fileLoaderRule,
-                test: /\.svg$/i,
-                resourceQuery: /url/,
-            },
-            {
-                test: /\.svg$/i,
-                issuer: fileLoaderRule.issuer,
-                resourceQuery: {
-                    not: [...fileLoaderRule.resourceQuery.not, /url/],
-                },
-                use: ['@svgr/webpack'],
-            },
-        );
+  reactStrictMode: false,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'pedixhwyfardtsanotrp.supabase.co',
+      },
+      {
+        protocol: 'https',
+        hostname: 'pbnegzqbzkddsnebarhp.supabase.co',
+      },
+      {
+        protocol: 'http',
+        hostname: '**.kakaocdn.net',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.googleusercontent.com',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.pstatic.net',
+      },
+      { protocol: 'https', hostname: 'encrypted-tbn3.gstatic.com' },
+      { protocol: 'https', hostname: 'example.com' },
+    ],
+  },
+  webpack(config) {
+    // Grab the existing rule that handles SVG imports
+    const fileLoaderRule = config.module.rules.find((rule) => rule.test?.test?.('.svg'));
+    config.module.rules.push(
+      {
+        ...fileLoaderRule,
+        test: /\.svg$/i,
+        resourceQuery: /url/,
+      },
+      {
+        test: /\.svg$/i,
+        issuer: fileLoaderRule.issuer,
+        resourceQuery: {
+          not: [...fileLoaderRule.resourceQuery.not, /url/],
+        },
+        use: ['@svgr/webpack'],
+      },
+    );
 
-        fileLoaderRule.exclude = /\.svg$/i;
+    fileLoaderRule.exclude = /\.svg$/i;
 
-        return config;
-    },
+    return config;
+  },
 };
 
-const nextConfigFunction = async phase => {
-    if (
-        phase === PHASE_DEVELOPMENT_SERVER ||
-        phase === PHASE_PRODUCTION_BUILD
-    ) {
-        const withPWA = (await import('next-pwa')).default({
-            dest: 'public',
-            register: true,
-            scope: '/',
-            sw: '/sw.js',
-            // buildExcludes: [/middleware-manifest\.json$/],
-            swSrc: '/sw.js',
-        });
-        return withPWA(nextConfig);
-    }
-    return nextConfig;
+const nextConfigFunction = async (phase) => {
+  if (phase === PHASE_DEVELOPMENT_SERVER || phase === PHASE_PRODUCTION_BUILD) {
+    const withPWA = (await import('next-pwa')).default({
+      dest: 'public',
+      register: true,
+      scope: '/',
+      sw: '/sw.js',
+      // buildExcludes: [/middleware-manifest\.json$/],
+      swSrc: '/sw.js',
+    });
+    return withPWA(nextConfig);
+  }
+  return nextConfig;
 };
 
 // const withPWA = withPWAInit({
