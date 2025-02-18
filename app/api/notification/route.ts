113c151dd0e5f211a4378dcc443d44844d420d35
Eun Oh <bdohhhhh@gmail.com>
2025-02-18 22:13:58 +0900

    [update]: notification route 수정중

--- app/api/notification/route.ts
+++ app/api/notification/route.ts
@@ -7,11 +7,41 @@ export async function GET() {
   const supabase = createClient();
 
   const {
+    data: { user },
+    error: userError,
+  } = await supabase.auth.getUser();
+
+  if (userError) {
+    return new Response(userError.message, { status: 500 });
+  }
+
+  if (!user) {
+    return new Response('User not found', { status: 404 });
+  }
+
+  const {
+    data: { buddy },
+    error: buddyError,
+  } = await supabase.from('buddies').select('*').eq('buddy_email', user.email).single();
+
+  if (buddyError) {
+    return new Response(buddyError.message, { status: 500 });
+  }
+
+  if (!buddy) {
+    return new Response('Buddy not found', { status: 404 });
+  }
+
+  const {
     data: notifications,
     error,
   }: { data: Notification[] | null; error: PostgrestError | null } = await supabase
     .from('notifications')
-    .select('*');
+    .select('*')
+    .eq('notification_isRead', false)
+    .eq('notification_receiver', buddy.buddy_id)
+    .neq('notification_sender', buddy.buddy_id)
+    .order('notification_created_at', { ascending: false });
 
   if (error) {
     return new Response(error.message, { status: 500 });
